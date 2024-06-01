a01e0e6ba64fdf04bbecac68bbb98b539b05e1f6
TCFII <168501077+TCFII@users.noreply.github.com>
2024-06-01 00:03:52 +0000

    completed

--- src/app.js
+++ src/app.js
@@ -7,5 +7,41 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Excuse Generator Project");
+  const whos = [
+    "The teacher",
+    "My brother",
+    "The dog",
+    "My boss",
+    "Uncle Fred"
+  ];
+  const whats = ["pooped", "ate", "burned", "hit me with", "slapped"];
+  const avoids = [
+    "the floor",
+    "a stick",
+    "the cookies",
+    "the work",
+    "his face"
+  ];
+  const whens = [
+    "last night.",
+    "yesterday.",
+    "last week.",
+    "a month ago.",
+    "a while back."
+  ];
+
+  const whosIndex = Math.floor(Math.random() * whos.length);
+  const who = whos[whosIndex];
+  const whatsIndex = Math.floor(Math.random() * whats.length);
+  const what = whats[whatsIndex];
+  const avoidsIndex = Math.floor(Math.random() * avoids.length);
+  const avoid = avoids[avoidsIndex];
+  const whensIndex = Math.floor(Math.random() * whens.length);
+  const when = whens[whensIndex];
+
+  // const randomExcuse = who + " " + what + " " + avoid + " " + when;
+  const randomExcuse = `${who} ${what} ${avoid} ${when}`;
+  // console.log(randomExcuse);
+  document.querySelector("h2").innerText = randomExcuse;
 };
